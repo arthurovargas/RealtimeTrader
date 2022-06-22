eb50c108c7e3763b9a612db8592bec0374724d85
unknown <Uniminuto@RSP-L90304-T-01.UMD.LOCAL.EDU>
2022-06-22 15:51:58 -0500

    Login Modify 1

--- app/src/main/java/com/project/realtimetrader/LoginActivity.kt
+++ app/src/main/java/com/project/realtimetrader/LoginActivity.kt
@@ -7,6 +7,12 @@ import android.view.View
 import android.widget.EditText
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.project.firestore.model.User
+import com.project.firestore.model.network.Callback
+import com.project.firestore.model.network.FirestoreService
+import com.project.firestore.model.network.USER_COLLECTION_NAME
+import java.lang.Exception
 
 const val USERNAME_KEY = "username_key"
 
@@ -17,21 +23,47 @@ class LoginActivity : AppCompatActivity() {
 
     private var auth: FirebaseAuth = FirebaseAuth.getInstance()
 
+    lateinit var firestoreService: FirestoreService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        firestoreService = FirestoreService(FirebaseFirestore.getInstance())
     }
 
     fun onStartClicked(view: View) {
+        view.isEnabled = false // Deshabilitar el boton Start
         val username : EditText = findViewById(R.id.username)
         auth.signInAnonymously().addOnCompleteListener { task ->
             if (task.isSuccessful){
                 username.text.toString()
+                val user = User()
+                user.username = username.toString()
+                saveUserAndStartMainActivity(user, view)
                 startMainActivity(username.text.toString())
             } else {
                 showErrorMessage(view)
+                view.isEnabled = true
             }
         }
+    }
+
+    private fun saveUserAndStartMainActivity(user: User, view: View) {
+        firestoreService.setDocument(
+            user,
+            USER_COLLECTION_NAME,
+            user.username,
+            object :
+                Callback<Void> {
+                override fun onSuccess(result: Void?) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onFailed(exception: Exception) {
+                    TODO("Not yet implemented")
+                }
+
+            })
     }
 
     private fun showErrorMessage(view: View) {
